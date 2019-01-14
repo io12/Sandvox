52563e22ab830c7e98ef92004cefcaf164e9f2ba
Benjamin Levy <blevy@protonmail.com>
2019-01-14 15:11:00 -0500

    Improve sand coloring

--- src/shaders/voxel_vert.glsl
+++ src/shaders/voxel_vert.glsl
@@ -11,7 +11,13 @@ void main(void) {
 	gl_Position = matrix * vec4(pos, 1.0);
 	switch (voxel_type) {
 	case 1u: // Sand
-		v_color = vec4(0.926, 0.785, 0.684, 1.0);
+		if (int(pos.x) % 2 == 0) {
+			v_color = vec4(0.926, 0.785, 0.684, 1.0);
+		} else if (int(pos.z) % 2 == 0) {
+			v_color = vec4(0.626, 0.585, 0.484, 1.0);
+		} else {
+			v_color = vec4(0.426, 0.385, 0.284, 1.0);
+		}
 		break;
 	default:
 		v_color = vec4(1.0, 1.0, 1.0, 1.0);
