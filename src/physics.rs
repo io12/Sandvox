fc0b2b47d9f1acf5880e38f2fb5764e0ac9d5eb4
Benjamin Levy <blevy@protonmail.com>
2019-01-14 11:00:38 -0500

    Increase voxel size again

--- src/physics.rs
+++ src/physics.rs
@@ -5,10 +5,10 @@ use clamp::clamp;
 use client::{GameState, Player, VOX_MAX_X, VOX_MAX_Y, VOX_MAX_Z};
 use render::VoxInd;
 
-const EYE_HEIGHT: f32 = 16.2; // Height of the player's eyes
-const FOREHEAD_SIZE: f32 = 2.0; // Vertical distance from the player's eyes to the top of the player
-const PLAYER_RADIUS: f32 = 3.0; // Radius of the player hitbox (cylinder)
-const ACCEL_GRAV: f32 = 100.0; // Acceleration due to gravity, in dm/s^2
+const EYE_HEIGHT: f32 = 1.62; // Height of the player's eyes
+const FOREHEAD_SIZE: f32 = 0.2; // Vertical distance from the player's eyes to the top of the player
+const PLAYER_RADIUS: f32 = 0.3; // Radius of the player hitbox (cylinder)
+const ACCEL_GRAV: f32 = 9.8; // Acceleration due to gravity, in m/s^2
 
 // Determine if the voxel at `pos` is a boundary (one voxel outside the voxel grid)
 fn boundary_at_pos(pos: Point3<f32>) -> bool {
